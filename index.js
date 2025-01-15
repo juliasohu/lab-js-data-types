75522da97190507594dd05d382d1481e09a3f167
Julia Solias <juliasohu@gmail.com>
2025-01-15 15:07:31 +0100

    Finished exercise

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+let tongTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`
 
 // Print out the concatenated string
-
+console.log(tongTwister)
 
 
 
@@ -23,10 +23,14 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+function upperLastLetter (i) {
+    let final = i.slice(0, i.length - 1) + i[i.length - 1].toUpperCase();
+    return final
+}
 
 // Print the cameLtaiL-formatted string
-
-
+let result = upperLastLetter(part1) + upperLastLetter(part2)
+console.log(result)
 
 
 /*******************************************
@@ -36,10 +40,11 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+let tipAmount = billTotal * 0.15
 
 // Print out the tipAmount
 
-
+console.log(tipAmount)
 
 
 /*******************************************
@@ -48,10 +53,10 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
-
+let randomNumber = Math.random()
 // Print the generated random number
 
-
+console.log(randomNumber)
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +67,29 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+//My reply: false
+console.log(expression1)
 
 const expression2 = a || b;
+//My reply: true
+console.log(expression2)
 
 const expression3 = !a && b;
+//My reply: false
+console.log(expression3)
 
 const expression4 = !(a && b);
+//My reply: true
+console.log(expression4)
 
 const expression5 = !a || !b;
+//My reply: true
+console.log(expression5)
 
 const expression6 = !(a || b);
+//My reply: false
+console.log(expression6)
 
 const expression7 = a && a;
+//My reply: true
+console.log(expression7)
